fa5adc7f881e813db433edbe3d648fd83d1b0cae
Tun Aung Kyaw <mr.tunaungkyaw96@gmail.com>
2023-03-31 08:32:15 +0630

    add Coming soon

--- src/Pages/List.js
+++ src/Pages/List.js
@@ -3,7 +3,7 @@ import React from "react";
 export const List = () => {
     return (
         <div>
-            <h1>Show Memory List</h1>
+            <h1>Show Memory List Coming Soon</h1>
         </div>
     );
 };
